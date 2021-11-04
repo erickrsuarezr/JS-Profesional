5e727c3686e414159e38ab34427b93a8d7097bd4
Erick Rodolfo <erickrsuarezr@hotmail.com>
2021-11-03 21:15:00 -0600

    Se crea una funcion para inicializar los plugins y dos clases más para hacer mute o unmute al video

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,13 +1,34 @@
 function MediaPlayer (config){
     this.media = config.el
-    }
-    
-    MediaPlayer.prototype.play = function(){
-    if(this.media.paused){
-    this.media.play();
-    } else {
-        this.media.pause()
-    }
-    }
+    this.plugins = config.plugins || [];
 
-    export default MediaPlayer;
+    this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function(){
+    this.plugins.forEach(plugin =>{
+        plugin.run(this);
+    })
+}
+    
+MediaPlayer.prototype.play = function(){
+if(this.media.paused){
+this.media.play();
+} else {
+    this.media.pause()
+}
+}
+
+MediaPlayer.prototype.mute = function(){
+    this.media.muted = true;
+}
+
+MediaPlayer.prototype.unmute = function(){
+    if (this.media.muted === true) {
+        this.media.muted = false;
+    } else {
+        this.media.muted = true;
+    }
+}
+
+export default MediaPlayer;
