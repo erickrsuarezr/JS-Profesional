364ae41cbf2061afa7f20e77b6da842b50c289fd
Erick Rodolfo <erickrsuarezr@hotmail.com>
2021-11-24 14:06:12 -0600

    Agregamos la linea para poder usar serviceworker en nuestra aplicacion y asi funcione con cache de manera offline

--- assets/index.js
+++ assets/index.js
@@ -12,3 +12,9 @@ const player =  new MediaPlayer({
 
 reproducir.onclick = () => player.togglePlay();
 mute.onclick = ()=> player.unmute();
+
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js').catch(error =>{
+        console.log(error.message);
+    })
+}
