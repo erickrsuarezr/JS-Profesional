f2f76febe190e2336d9880fb8c862aa6d71c9991
Erick Rodolfo <erickrsuarezr@hotmail.com>
2021-11-27 14:08:17 -0600

    Cambio de nombre, import.meta. no encuentro solucion

---
+++ assets/index.ts
@@ -0,0 +1,20 @@
+import MediaPlayer from './MediaPlayer'
+import Autoplay from './plugins/Autoplay'
+import Autopause from './plugins/Autopause'
+
+const video = document.querySelector("video")
+const reproducir:HTMLElement = document.querySelector(".Play-pause")
+const mute:HTMLElement = document.querySelector(".Mute-unmute");
+const player =  new MediaPlayer({
+    el: video, 
+    plugins:[new Autoplay(), new Autopause()]
+});
+
+reproducir.onclick = () => player.togglePlay();
+mute.onclick = ()=> player.unmute();
+
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register(new URL('/sw.js',  import.meta.url)).catch(error =>{
+        console.log(error.message);
+    })
+}
