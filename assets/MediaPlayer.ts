f3c9ab3dd83e0acb8935a7ec70ee0109dd6b49aa
Erick Rodolfo <erickrsuarezr@hotmail.com>
2021-12-03 14:26:59 -0600

    Se crea a un div al rededor de video usando JS

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,12 +1,22 @@
 class MediaPlayer {
     media: HTMLMediaElement;
     plugins: Array<any>;
+    container: HTMLElement;
     
     constructor(config) {
         this.media = config.el;
         this.plugins = config.plugins || [];
+        this.initPlayer();
         this.initPlugins();
     }
+    initPlayer(){
+        this.container = document.createElement('div');
+
+        this.media.parentNode.insertBefore(this.container, this.media);//contenedor a lado de media
+
+        this.container.appendChild(this.media);//Media dentro de contenedor
+    }
+
     private initPlugins() {
         this.plugins.forEach(plugin => {
             plugin.run(this);
